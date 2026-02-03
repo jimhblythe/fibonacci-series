549a9f06719e0332698b3d1ab35b20aba6a3c08d
Jim Blythe <jim.h.blythe@gmail.com>
2026-02-03 12:34:18 -0800

    Add SQL Language

--- Languages.md
+++ Languages.md
@@ -8,11 +8,13 @@ Hello there!!
 
 ## Code already developed in this programming language.
 
+- BASIC
 - Bash
 - C
 - C#
 - C++
 - Dart
+- Fortran
 - Go
 - Haskell
 - Java
@@ -21,12 +23,14 @@ Hello there!!
 - Lua
 - Matlab
 - Miranda
+- Octave
 - PHP
 - Powershell Script
 - Python
 - R
 - Ruby
 - Rust
+- SQL
 - Scala
 - Scheme
 - Typescript
---
+++ fibonacci_series/SQL/SQL_fibonacci.sql
@@ -0,0 +1,40 @@
+--Tested in Visual Studio Code using SQLite extension by alexcvzz
+-- Does not need a real db, choose memory
+
+--change this value to the highest desired fib(n)
+create table nth as select 100;
+
+--return only the nth value
+with recursive fibCTE(n, fib, prior) as
+     (select 0 as n, 0 as fib, 0 as prior
+     union 
+     select 1 as n, 1 as fib, 0 as prior
+     union
+     select n + 1,
+        fib + prior,
+        fib
+     from fibCTE f
+     where n > 0 and n < (select * from nth limit 1)
+    )
+
+select n, fib
+from fibCTE
+order by n desc
+limit 1;
+
+--return fib(0) through the nth value
+with recursive fibCTE(n, fib, prior) as
+     (select 0 as n, 0 as fib, 0 as prior
+     union 
+     select 1 as n, 1 as fib, 0 as prior
+     union
+     select n + 1,
+        fib + prior,
+        fib
+     from fibCTE f
+     where n > 0 and n < (select * from nth limit 1)
+    )
+
+select n, fib
+from fibCTE
+order by n;
